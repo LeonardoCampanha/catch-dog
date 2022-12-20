167f29a6ff6b3b775011b37170d551ce3420e922
Leonardo Campanha <ileonardo@outlook.com>
2022-12-20 16:42:05 +0000

    fixing structure

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const url = "https://dog.ceo/api/breeds/image/random";
 let ImgEl = document.querySelector(".hero");
 
 async function getDog() {
-	ImgEl.innerHTML = `<img src="giphy.gif" alt="Loading">`;
+	ImgEl.innerHTML = `<img src="images/giphy.gif" alt="Loading">`;
 	const response = await fetch(url);
 	const data = await response.json();
 
